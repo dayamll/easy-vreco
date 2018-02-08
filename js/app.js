1df3df390ae1d599908cc6145735a3856cd5befd
margb13 <marg.b13@hotmail.com>
2018-02-07 22:42:18 -0500

    Añadiendo funcionalidad de API Geolocation

--- js/app.js
+++ js/app.js
@@ -1,65 +1,64 @@
-var myLocation;
+function initMap() {
+  const containerMap = document.getElementById('map');
+  const directionsService = new google.maps.DirectionsService();
+  const directionsDisplay = new google.maps.DirectionsRenderer();
+  const geocoder = new google.maps.Geocoder();
+  // Punto de muestra en el mapa al carga la pagina
+  const losOlivos = { 
+    lat: -11.9594, 
+    lng: -77.0760 
+  };
 
-var initMap = (function () {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: -34.397,
-      lng: 150.644
+  // Caracteristicas del mapa 
+  const mapOptions = {
+    zoom: 15,
+    center: losOlivos,
+  };
+
+  // Creamos el mapa con las constantes containerMap y 
+  const map = new google.maps.Map(containerMap, mapOptions);
+
+  // Adjuntamos al mapa las siguientes caracteriristicas
+  directionsDisplay.setMap(map);
+  // Aqui indicamos el efecto que tendra el marcador  
+  const markerMap = new google.maps.Marker({
+    position: { 
+      lat: losOlivos.lat, 
+      lng: losOlivos.lng 
     },
-    zoom: 14
-  });
-  var infoWindow = new google.maps.InfoWindow({
+    animation: google.maps.Animation.DROP,
     map: map
   });
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      myLocation = pos;
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      var thisPlace = new google.maps.LatLng(myLocation.lat, myLocation.lng);
-      var service = new google.maps.places.PlacesService(map);
-      service.nearbySearch({
-        location: thisPlace,
-        radius: 1500,
-        type: ["food"]
-      }, callback);
 
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      };
+  // Realizamos el llamado al evento load para que pueda ubicarme al momento que carge la pagina
+  window.addEventListener('load', function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getCoords, errorFound);
+    } else {
+      alert('Tu navegador no soporta el API de Geolocation');
+    }
+  });
 
-      function createMarker(thisPlace) {
-        var placeLoc = thisPlace.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: thisPlace.geometry.location
-        });
-        google.maps.event.addListener(marker, 'click', function () {
-          infoWindow.setContent(thisPlace.name);
-          infoWindow.open(map, this);
-        });
-      };
+  function errorFound(error) {
+    alert('Un error ocurrió: ' + error.code);
+  };
+  
+  function getCoords(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
 
-      map.setCenter(pos);
-    }, function () {
-      handleLocationError(true, infoWindow, map.getCenter());
+    let myLocation = new google.maps.Marker({
+      position: { 
+        lat: lat, 
+        lng: lon 
+      },
+      animation: google.maps.Animation.DROP,
+      map: map
     });
-  } else {
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-});
-
-var handleLocationError = (function (browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-    'Error: The Geolocation service failed.' :
-    'Error: Your browser doesn\'t support geolocation.');
-});
+    // Al mapa creado le agrego mi ubicación
+    map.setCenter({ 
+      lat: lat, 
+      lng: lon 
+    });
+  };
+}
