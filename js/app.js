5a793e0bbc5d5ff8743f582362d53acba54b28b3
margb13 <marg.b13@hotmail.com>
2018-02-08 00:13:26 -0500

    Añadiendo funcionalidad de trazado de ruta

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ function initMap() {
   const directionsDisplay = new google.maps.DirectionsRenderer();
   const geocoder = new google.maps.Geocoder();
   // Punto de muestra en el mapa al carga la pagina
-  const losOlivos = { 
-    lat: -11.9594, 
-    lng: -77.0760 
+  const losOlivos = {
+    lat: -11.9594,
+    lng: -77.0760
   };
 
   // Caracteristicas del mapa 
@@ -15,23 +15,22 @@ function initMap() {
     center: losOlivos,
   };
 
-  // Creamos el mapa con las constantes containerMap y 
+  // Creamos el mapa con las constantes containerMap y caracteristicas
   const map = new google.maps.Map(containerMap, mapOptions);
-
-  // Adjuntamos al mapa las siguientes caracteriristicas
   directionsDisplay.setMap(map);
+
   // Aqui indicamos el efecto que tendra el marcador  
   const markerMap = new google.maps.Marker({
-    position: { 
-      lat: losOlivos.lat, 
-      lng: losOlivos.lng 
+    position: {
+      lat: losOlivos.lat,
+      lng: losOlivos.lng
     },
     animation: google.maps.Animation.DROP,
     map: map
   });
 
   // Realizamos el llamado al evento load para que pueda ubicarme al momento que carge la pagina
-  window.addEventListener('load', function() {
+  window.addEventListener('load', function () {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(getCoords, errorFound);
     } else {
@@ -42,23 +41,56 @@ function initMap() {
   function errorFound(error) {
     alert('Un error ocurrió: ' + error.code);
   };
-  
+
   function getCoords(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
 
     let myLocation = new google.maps.Marker({
-      position: { 
-        lat: lat, 
-        lng: lon 
+      position: {
+        lat: lat,
+        lng: lon
       },
       animation: google.maps.Animation.DROP,
       map: map
     });
     // Al mapa creado le agrego mi ubicación
-    map.setCenter({ 
-      lat: lat, 
-      lng: lon 
+    map.setCenter({
+      lat: lat,
+      lng: lon
     });
   };
+
+  // Autocompletamos las direcciones de origen y destino
+  let originPoint = document.getElementById('input-origin');
+  let destinationPoint = document.getElementById('input-destination');
+
+  // usando la libraría Autocomplete para el autocompletado de los lugares
+  new google.maps.places.Autocomplete(originPoint);
+  new google.maps.places.Autocomplete(destinationPoint);
+
+  // Evento que traza la ruta y obtiene información de lo ruta,
+  let btnRuta = document.getElementById('trace-route');
+  btnRuta.addEventListener('click', trazarRuta);
+
+  function trazarRuta(e) {
+    e.preventDefault();
+    let request = {
+      origin: originPoint.value,
+      destination: destinationPoint.value,
+      travelMode: 'DRIVING',
+    }
+
+    function callback(result, status) {
+
+      if (status === 'OK') {
+        directionsDisplay.setDirections(result);
+        let distancia = result.routes[0].legs[0].distance.value;
+      } else {
+        window.alert('No encontramos la ruta');
+      }
+    }
+
+    directionsService.route(request, callback);
+  }
 }
